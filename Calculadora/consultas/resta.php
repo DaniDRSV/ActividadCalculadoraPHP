529c1315fd5850315bba6a2a12c1d5ce0b357676
DaniDRSV <127716687+DaniDRSV@users.noreply.github.com>
2023-04-10 11:26:46 -0600

    Add files via upload

--- Calculadora/consultas/resta.php
+++ Calculadora/consultas/resta.php
@@ -3,7 +3,7 @@ class Calculadora{
     public $numero1;
     public $numero2;
 
-    public function __constract($numero1, $numero2)
+    public function __construct($numero1, $numero2)
     {
         $this->numero1 = $numero1;
         $this->numero2 = $numero2;
